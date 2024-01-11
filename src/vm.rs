2b11d649a06e6efe68713a16463eff0e7cefe10b
Asier Pereiro <asier.pereiro.castro@cern.ch>
2024-01-11 19:27:43 +0100

    Add main loop and opcode decodification

--- src/vm.rs
+++ src/vm.rs
@@ -1,10 +1,44 @@
+use crate::instruction::Opcode;
+
 pub struct VirtualMachine {
-    registers: [i32; 32],
+    registers: [i32; 32], // register set
+    pc: usize,            // program counter
+    program: Vec<u8>,     // vector to store the bytecode
 }
 
 impl VirtualMachine {
     pub fn new() -> VirtualMachine {
-        VirtualMachine { registers: [0; 32] }
+        VirtualMachine {
+            registers: [0; 32],
+            pc: 0,
+            program: vec![],
+        }
+    }
+
+    pub fn decode_opcode(&mut self) -> Opcode {
+        let opcode = Opcode::from(self.program[self.pc]);
+        self.pc += 1;
+        return opcode;
+    }
+
+    pub fn run(&mut self) {
+        loop {
+            // The program counter must be within the program
+            if self.pc >= self.program.len() {
+                break;
+            }
+
+            match self.decode_opcode() {
+                Opcode::HLT => {
+                    println!("Executing HLT");
+                    return;
+                }
+                _ => {
+                    println!("Opcode not valid. Terminating.");
+                    return;
+                }
+            }
+        }
     }
 }
 

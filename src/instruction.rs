936b11708b5abe47a5509582a29fc935241c5fbc
Asier Pereiro <asier.pereiro.castro@cern.ch>
2024-01-11 19:12:12 +0100

    Add instruction and opcode

---
+++ src/instruction.rs
@@ -0,0 +1,28 @@
+#[derive(Debug, PartialEq)]
+pub enum Opcode {
+    HLT,
+    IGL,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Instruction {
+    opcode: Opcode,
+}
+
+impl Instruction {
+    pub fn new(opcode: Opcode) -> Instruction {
+        Instruction { opcode: opcode }
+    }
+}
+
+#[test]
+fn test_create_hlt() {
+    let opcode = Opcode::HLT;
+    assert_eq!(opcode, Opcode::HLT);
+}
+
+#[test]
+fn test_create_instruction() {
+    let instruction = Instruction::new(Opcode::HLT);
+    assert_eq!(instruction.opcode, Opcode::HLT);
+}

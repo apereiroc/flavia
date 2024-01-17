da500db40c9fb3767a5a094b77added7107440d8
Asier Pereiro <asier.pereiro.castro@cern.ch>
2024-01-17 18:54:18 +0100

    Add module for assembler

--- src/assembler/mod.rs
+++ src/assembler/mod.rs
@@ -1,6 +1,8 @@
 use crate::instruction::Opcode;
+pub mod instruction_parsers;
 pub mod opcode_parsers;
 pub mod operand_parsers;
+pub mod program_parsers;
 pub mod register_parsers;
 
 #[derive(Debug, PartialEq)]

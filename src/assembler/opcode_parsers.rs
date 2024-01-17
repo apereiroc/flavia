5291873570e6a7ff568d7d9e8465449c232a376b
Asier Pereiro <asier.pereiro.castro@cern.ch>
2024-01-18 00:35:06 +0100

    Handle general opcode

--- src/assembler/opcode_parsers.rs
+++ src/assembler/opcode_parsers.rs
@@ -1,21 +1,28 @@
 use crate::assembler::Token;
 use crate::instruction::Opcode;
+use nom::alpha1;
 use nom::types::CompleteStr;
 
-named!(pub opcode_load<CompleteStr, Token>,
-    do_parse!(
-        tag_no_case!("load") >> (Token::Op{ code: Opcode::LOAD})
-    )
+named!(pub opcode<CompleteStr, Token>,
+  do_parse!(
+      opcode: alpha1 >>
+      (
+        {
+            Token::Op{code: Opcode::from(opcode)}
+        }
+      )
+  )
 );
 
 #[test]
 fn test_opcode_load() {
-    let result = opcode_load(CompleteStr("load"));
+    let result = opcode(CompleteStr("load"));
     assert_eq!(result.is_ok(), true);
     let (rest, token) = result.unwrap();
     assert_eq!(token, Token::Op { code: Opcode::LOAD });
     assert_eq!(rest, CompleteStr(""));
 
-    let result = opcode_load(CompleteStr("aold"));
-    assert_eq!(result.is_ok(), false);
+    let result = opcode(CompleteStr("aold"));
+    let (_, token) = result.unwrap();
+    assert_eq!(token, Token::Op { code: Opcode::IGL });
 }

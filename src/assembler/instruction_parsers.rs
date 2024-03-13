589de404287ae4d080520ccba56ae414a4be08b3
Asier Pereiro <asier.pereiro.castro@cern.ch>
2024-03-13 03:10:00 +0100

    Handle symbols and fix clippy

--- src/assembler/instruction_parsers.rs
+++ src/assembler/instruction_parsers.rs
@@ -6,6 +6,8 @@ use crate::assembler::Token;
 
 use nom::types::CompleteStr;
 
+use super::symbols::SymbolTable;
+
 #[derive(Debug, PartialEq)]
 pub struct AssemblerInstruction {
     pub opcode: Option<Token>,
@@ -17,37 +19,37 @@ pub struct AssemblerInstruction {
 }
 
 impl AssemblerInstruction {
-    pub fn to_bytes(&self) -> Vec<u8> {
+    pub fn to_bytes(&self, symbols: &SymbolTable) -> Vec<u8> {
         let mut results: Vec<u8> = vec![];
 
         if let Some(ref token) = self.opcode {
             match token {
-                Token::Op { code } => match code {
-                    _ => {
-                        let b: u8 = *code as u8;
-                        results.push(b);
-                    }
-                },
+                Token::Op { code } => {
+                    let b: u8 = *code as u8;
+                    results.push(b);
+                }
                 _ => {
                     println!("Non-opcode found in opcode field");
                 }
             }
         }
 
-        for operand in &[&self.operand1, &self.operand2, &self.operand3] {
-            if let Some(token) = operand {
-                AssemblerInstruction::extract_operand(token, &mut results);
-            }
-        }
+        [&self.operand1, &self.operand2, &self.operand3]
+            .iter()
+            .for_each(|operand| {
+                if let Some(token) = operand {
+                    AssemblerInstruction::extract_operand(token, &mut results, symbols);
+                }
+            });
 
         while results.len() < 4 {
             results.push(0_u8);
         }
 
-        return results;
+        results
     }
 
-    fn extract_operand(t: &Token, results: &mut Vec<u8>) {
+    fn extract_operand(t: &Token, results: &mut Vec<u8>, symbols: &SymbolTable) {
         match t {
             Token::Register { reg_num } => {
                 results.push(*reg_num);
@@ -56,13 +58,39 @@ impl AssemblerInstruction {
                 let converted = *value as i16;
                 let byte1: u8 = converted as u8;
                 let byte2: u8 = (converted >> 8).try_into().unwrap();
-                results.push(byte2 as u8);
-                results.push(byte1 as u8);
+                results.push(byte2);
+                results.push(byte1);
             }
+            Token::LabelUsage { name } => {
+                if let Some(value) = symbols.symbol_value(name) {
+                    let byte1 = value;
+                    let byte2 = value >> 8;
+                    results.push(byte2 as u8);
+                    results.push(byte1 as u8);
+                }
+            }
+
             _ => {
                 println!("Opcode found in operand field");
                 std::process::exit(1);
             }
+        }
+    }
+
+    pub fn is_label(&self) -> bool {
+        self.label.is_some()
+    }
+
+    pub fn label_name(&self) -> Option<String> {
+        match &self.label {
+            Some(l) => {
+                if let Token::LabelDeclaration { name } = l {
+                    Some(name.clone())
+                } else {
+                    None
+                }
+            }
+            None => None,
         }
     }
 }
@@ -75,7 +103,6 @@ named!(instruction_combined<CompleteStr, AssemblerInstruction>,
         o2: opt!(operand) >>
         o3: opt!(operand) >>
         (
-            {
             AssemblerInstruction{
                 opcode: Some(o),
                 label: l,
@@ -83,7 +110,6 @@ named!(instruction_combined<CompleteStr, AssemblerInstruction>,
                 operand1: o1,
                 operand2: o2,
                 operand3: o3,
-            }
             }
         )
     )
@@ -119,6 +145,64 @@ mod tests {
                     directive: None,
                     operand1: Some(Token::Register { reg_num: 0 }),
                     operand2: Some(Token::IntegerOperand { value: 100 }),
+                    operand3: None
+                }
+            ))
+        );
+    }
+
+    #[test]
+    fn test_parse_instruction_form_two() {
+        let result = instruction_combined(CompleteStr("hlt"));
+        assert_eq!(
+            result,
+            Ok((
+                CompleteStr(""),
+                AssemblerInstruction {
+                    opcode: Some(Token::Op { code: Opcode::HLT }),
+                    label: None,
+                    directive: None,
+                    operand1: None,
+                    operand2: None,
+                    operand3: None
+                }
+            ))
+        );
+    }
+
+    #[test]
+    fn test_parse_instruction_form_three() {
+        let result = instruction_combined(CompleteStr("add $0 $1 $2\n"));
+        assert_eq!(
+            result,
+            Ok((
+                CompleteStr(""),
+                AssemblerInstruction {
+                    opcode: Some(Token::Op { code: Opcode::ADD }),
+                    label: None,
+                    directive: None,
+                    operand1: Some(Token::Register { reg_num: 0 }),
+                    operand2: Some(Token::Register { reg_num: 1 }),
+                    operand3: Some(Token::Register { reg_num: 2 }),
+                }
+            ))
+        );
+    }
+    #[test]
+    fn test_parse_instruction_form_one_with_label() {
+        let result = instruction_combined(CompleteStr("load $0 @test1\n"));
+        assert_eq!(
+            result,
+            Ok((
+                CompleteStr(""),
+                AssemblerInstruction {
+                    opcode: Some(Token::Op { code: Opcode::LOAD }),
+                    label: None,
+                    directive: None,
+                    operand1: Some(Token::Register { reg_num: 0 }),
+                    operand2: Some(Token::LabelUsage {
+                        name: "test1".to_string()
+                    }),
                     operand3: None
                 }
             ))
